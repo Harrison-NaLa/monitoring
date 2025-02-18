837e7d7a2d08448b98320be1bf6469f77fc007cf
Harrison-Naranjo <harrisonsteven921120@gmail.com>
2025-02-18 11:06:00 -0500

    fix: delete unused components

--- src/app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import {getTrackData} from '@/app/tracks/store/tracks.request';
-
-export async function GET() {
-    const msm = 'Hello, Next.js!'
-    const track = await getTrackData<{name: string, artists: any[]}>('/5oFX4zn4CIDN0l2RCVuObT');
-    return Response.json({track})
-}
